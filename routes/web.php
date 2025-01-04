caa2c8e65c4c7e5ede06b1430d322ac4fa1827cc
Jake Klassen <jklassendev@gmail.com>
2025-01-04 18:04:35 -0500

    lockdown php version, configure laravel php and add basic socialite setup

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/auth/google/redirect', function () {
+    return Socialite::driver('google')->redirect();
+});
+
+Route::get('/auth/google/callback', function () {
+    $user = Socialite::driver('google')->user();
+    dd($user);
 });
